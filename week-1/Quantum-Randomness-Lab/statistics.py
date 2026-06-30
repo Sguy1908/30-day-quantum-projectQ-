6c42b58ba7cc166ab6b2e2d566768b9b4d52c92c
sguy1908 <shlokgoenka2508@gmail.com>
2026-06-30 17:31:06 +0530

    minor changes to statistics.py

--- week-1/Quantum-Randomness-Lab/statistics.py
+++ week-1/Quantum-Randomness-Lab/statistics.py
@@ -2,11 +2,14 @@ from qiskit import QuantumCircuit
 from qiskit_aer import AerSimulator
 import numpy as np
 
+#creating the simulator, applying hadamard and measuring
 simulator = AerSimulator() 
 qc = QuantumCircuit(1,1)
 qc.h(0) 
 qc.measure(0,0)
 
+
+#running it 1000 times and obtaining the results
 job = simulator.run(qc, shots=1000)
 result = job.result() 
 
@@ -18,5 +21,6 @@ ones = counts.get("1",0)
 #printing outputs
 print("Zeros:", zeros)
 print("Ones:", ones) 
+#calculating mean and standard deviation
 print("Mean:", np.mean([zeros,ones]))
 print("Standard Deviation:", np.std([zeros,ones])) 
