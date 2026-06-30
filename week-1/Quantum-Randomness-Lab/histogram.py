ea3777d703300ddb8fc8b92df246e97199b6933d
sguy1908 <shlokgoenka2508@gmail.com>
2026-06-30 17:28:31 +0530

    minor changes to code

--- week-1/Quantum-Randomness-Lab/histogram.py
+++ week-1/Quantum-Randomness-Lab/histogram.py
@@ -3,17 +3,21 @@ from qiskit_aer import AerSimulator
 from qiskit.visualization import plot_histogram 
 import matplotlib.pyplot as plt 
 
-
+#creating the simulator
 simulator = AerSimulator() 
 qc = QuantumCircuit(1,1) 
 
+
+#applying superposition and measuring 
 qc.h(0)
 qc.measure(0,0) 
 
 job = simulator.run(qc, shots=1000)
 
+#obtaining the results
 result = job.result() 
 counts = result.get_counts() 
 
+#generating the histogram
 plot_histogram(counts) 
 plt.show() 
